3e51c1e7b27039ca183f5598c643373439a3e978
Ottosson93 <Henkan93@gmail.com>
2023-10-12 14:12:14 +0200

    Setting menu functionality
    Added functionality to Settings menu

--- SomniatProject/Assets/Scripts/UI/Menus/SettingsMenuView.cs
+++ SomniatProject/Assets/Scripts/UI/Menus/SettingsMenuView.cs
@@ -2,12 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Audio;
 public class SettingsMenuView : Menu
 {
     [SerializeField] private Button _backButton;
+
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+    private void Awake()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for(int i=0; i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height) currentResolutionIndex = i;
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
     public override void Initialize()
     {
         _backButton.onClick.AddListener(() => MenuManager.ShowLast());
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
