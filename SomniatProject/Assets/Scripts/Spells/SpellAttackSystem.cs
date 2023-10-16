7817bd6545efeeb280629f2854c2a67fdecc606c
plsegott <baetsailray@hotmail.com>
2023-10-16 12:15:08 +0200

    Added Spell Items
    Consumeable Spell Items

--- SomniatProject/Assets/Scripts/Spells/SpellAttackSystem.cs
+++ SomniatProject/Assets/Scripts/Spells/SpellAttackSystem.cs
@@ -19,6 +19,12 @@ public class PlayerLuciditySystem : MonoBehaviour
 
     private InputAction spellInput;
 
+
+    public void UpdateSpell(Spell spell)
+    {
+        spellToCast = spell;
+    }
+
     private void Awake()
     {
         controller = GetComponent<ThirdPersonController>();
