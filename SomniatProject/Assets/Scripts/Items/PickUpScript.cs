7c79dbf5cb63202009c5087169eab4f5ca918fb1
plsegott <baetsailray@hotmail.com>
2023-10-16 15:52:09 +0200

    Improved Pick-up Script
    Re added fading in of the button and improved pick-up script.
    
    Om ej behovs ta bort funktion vid behov.

--- SomniatProject/Assets/Scripts/Items/PickUpScript.cs
+++ SomniatProject/Assets/Scripts/Items/PickUpScript.cs
@@ -7,91 +7,59 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Animations;
 using Random = UnityEngine.Random;
+using Unity.VisualScripting;
+using UnityEditor.Timeline.Actions;
 
 public class PickUpScript : MonoBehaviour
 {
-    public DamgeTextPlayer damageTextPlayer;
-     Transform player;
+    Transform player;
     public float pickUpRange = 1.5F;
     private Transform eKeyPlane;
     private Material material;
-    Vector3 distanceToPlayer;
-    bool displayKey, playerInRange;
-
-
+    bool displayKey;
 
     void Start()
     {
         displayKey = false;
-        playerInRange = false;
         eKeyPlane = transform.GetChild(0);
         material = eKeyPlane.GetComponent<MeshRenderer>().material;
-       // player = FindObjectOfType<Player>().transform;
         player = GameObject.FindGameObjectWithTag("Player").transform;
-        
+
     }
 
-    // Update is called once per frame
-    public async Task<bool> PickUp()
+    public async Task Update()
     {
-        distanceToPlayer = player.position - transform.position;
-
-        if (distanceToPlayer.magnitude <= pickUpRange)
+        if (InRangeOfPickup)
         {
-            
-
-            playerInRange = true;
-
-          //  if (!displayKey && CanIncreaseOpacity)
-            //    await ShowKey();
-
-            if (Keyboard.current.eKey.wasReleasedThisFrame)
-            {
-                Debug.Log("Pressed E");
-                damageTextPlayer.AddHealth(Random.Range(10, 100) , transform);
-                return true;
-            }
+            if (!displayKey && CanIncreaseOpacity)
+                await ChangeKeyOpacity(IncreaseOpacity);
         }
         else
         {
-            playerInRange = false;
-
-            //if (!displayKey && CanDecreaseOpacity)
-              //  await HideKey();
+            if (!displayKey && CanDecreaseOpacity)
+                await ChangeKeyOpacity(DecreaseOpacity);
         }
-        //Debug.Log("Return False");
-        return false;
-    }
-
-    private async Task ShowKey()
-    {
-        Debug.Log("ShowKey");
-        displayKey = true;
-        while (CanIncreaseOpacity && playerInRange)
-        {
-            material.color = IncreaseOpacity;
-            await Task.Delay(10);
-        }
-        displayKey = false;
 
     }
-    private async Task HideKey()
+
+    private async Task ChangeKeyOpacity(Color targetColor)
     {
-        Debug.Log("HideKey");
         displayKey = true;
-        while (CanDecreaseOpacity && !playerInRange)
+
+        while ((CanIncreaseOpacity && targetColor == IncreaseOpacity) ||
+               (CanDecreaseOpacity && targetColor == DecreaseOpacity))
         {
-            material.color = DecreaseOpacity;
-            await Task.Delay(10);
+            material.color = targetColor;
+            await Task.Delay(2);
         }
+
         displayKey = false;
     }
-
+    public bool PickUp => InRangeOfPickup ? Keyboard.current.eKey.wasReleasedThisFrame ? true : false : false;
+    public bool InRangeOfPickup => (player.position - transform.position).magnitude <= pickUpRange;
     private Color DecreaseOpacity => CanDecreaseOpacity ? new Color(material.color.r, material.color.g, material.color.b, (material.color.a - 0.01F)) : material.color;
     private Color IncreaseOpacity => CanIncreaseOpacity ? new Color(material.color.r, material.color.g, material.color.b, (material.color.a + 0.01F)) : material.color;
-
     private bool CanIncreaseOpacity => material.color.a <= 1;
-
     private bool CanDecreaseOpacity => material.color.a >= 0.01F;
 
 }
