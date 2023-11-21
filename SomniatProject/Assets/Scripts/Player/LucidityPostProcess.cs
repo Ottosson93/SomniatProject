17b7e14f36ae44afa99acfa53fadce35617772b6
plsegott <baetsailray@hotmail.com>
2023-11-21 05:52:37 +0100

    Shade finally done

---
+++ SomniatProject/Assets/Scripts/Player/LucidityPostProcess.cs
@@ -0,0 +1,26 @@
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class LucidityPostProcess : MonoBehaviour
+{
+
+    private Transform lucidCamera;
+    private Player player;
+    public float minScale = 0.01f; // Initial radius when lucidity is at its lowest
+    public float maxScale = 3.0f;    // Minimum radius when lucidity is at its max
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        lucidCamera = GameObject.FindGameObjectWithTag("Mask").GetComponent<Transform>();
+    }
+
+    public void UpdateLucidityMask(float lucidty)
+    {
+        float radius = Mathf.Lerp(minScale, maxScale, lucidty / player.maxLucidity);
+       
+        lucidCamera.localScale = new Vector3(radius, radius, radius);
+    }
+
+    
+}
