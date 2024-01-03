c5265b264334ab97e7657d0fef7f1e8c757e7621
Mindeusa <mindaugas.zaidzia@gmail.com>
2024-01-03 12:13:20 +0100

    range for aggro

--- SomniatProject/Assets/Scripts/AI/BehaviorTrees/GuardMeleeBT.cs
+++ SomniatProject/Assets/Scripts/AI/BehaviorTrees/GuardMeleeBT.cs
@@ -11,8 +11,8 @@ public class GuardMeleeBT : Tree
 
     public static float speed = 2f;
     public static float targetedSpeed = 4f;
-    public static float fovRange = 7f;
-    public static float distance = 9f;
+    public static float fovRange = 15f;
+    public static float distance = 20f;
     public static float attackRange = 2.5f;
     public static float rotationSpeed = 3f;
     public static float attackDamage = 10f;
