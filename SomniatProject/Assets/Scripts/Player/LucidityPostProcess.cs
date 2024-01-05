061fcd061a10918025ce9901b1a84c372a7d9e12
plsegott <baetsailray@hotmail.com>
2024-01-05 03:03:15 +0100

    fix proj

--- SomniatProject/Assets/Scripts/Player/LucidityPostProcess.cs
+++ SomniatProject/Assets/Scripts/Player/LucidityPostProcess.cs
@@ -9,7 +9,7 @@ public class LucidityPostProcess : MonoBehaviour
     private Player player;
     public float minScale; // Initial radius when lucidity is at its lowest
     public float maxScale;    // Minimum radius when lucidity is at its max
-
+    private float minCapsuleScale = 0.005F;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
@@ -21,9 +21,11 @@ public class LucidityPostProcess : MonoBehaviour
     {
       //  Debug.Log($"KVOTEN: {lucidity / player.maxLucidity}");
         float radius = Mathf.Lerp(minScale, maxScale, lucidity / player.maxLucidity);
- //       Debug.Log($"KVOTEN: {lucidity / player.maxLucidity} RADIUS: {radius}");
+        float capsuleRadius = Mathf.Lerp(minCapsuleScale, maxScale, lucidity / player.maxLucidity);
+
+        //       Debug.Log($"KVOTEN: {lucidity / player.maxLucidity} RADIUS: {radius}");
         lucidCamera.localScale = new Vector3(radius*1.10F, radius*0.65F, radius);  
-        lucidCapsule.localScale = new Vector3(radius * 1.10F, radius * 0.65F, radius);
+        lucidCapsule.localScale = new Vector3(capsuleRadius * 1.10F, capsuleRadius * 0.65F, capsuleRadius);
     }
 
     
