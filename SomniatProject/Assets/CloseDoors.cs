7efa8a0306de9a1cf18ee6b036f3c470dcb107e4
Mechalele <bennebladh@gmail.com>
2024-01-05 11:14:05 +0100

    Revert "Merge pull request #144 from Ottosson93/Battle"
    This reverts commit acb0dca9811e1d74a972284055e8ee963fdf8601, reversing
    changes made to d3a7e0b1e5489015c4b79000c60bf511732d5075.

--- SomniatProject/Assets/CloseDoors.cs
+++ SomniatProject/Assets/CloseDoors.cs
@@ -1,158 +1,78 @@
+using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CloseDoors : MonoBehaviour
 {
-    private enum State
-    {
-        Idle,
-        Closing,
-        Spawning,
-        Opening,
-        Completed
-    }
-
     [SerializeField] private GameObject jailDoor1, jailDoor2;
     [SerializeField] private Transform startPos1, startPos2, destination1, destination2;
-    [SerializeField] private GameObject[] spawnLocation;
-    private int enemiesToKill;
-    private bool spawning = false;
-    private float speed = 8, timer;
-    [SerializeField] private float timerTilDoorOpens;
-    private EnemySpawner enemySpawner;
-
-    private State state;
-
-    private void Awake()
-    {
-        state = State.Idle;
-    }
+    [SerializeField] private List<GameObject> enemies;
+    int enemiesToKill;
+    bool closing = false;
+    bool opening = false;
+    private float speed = 8;
+    float timer;
+    bool complete = false;
 
     private void Start()
     {
-        for (int i = 0; i < spawnLocation.Length; i++)
-        {
-            enemySpawner = spawnLocation[i].GetComponent<EnemySpawner>();
-            Debug.Log("getting locations " + enemySpawner.name);
-        }
-        
-        enemiesToKill = enemySpawner.waveNumber * spawnLocation.Length;
-        Debug.Log("Enemies to kill " + enemiesToKill);
+        enemiesToKill = enemies.Count;
     }
     void Update()
     {
-        if (state != State.Completed)
+        if (complete == false)
         {
-            //Closes the hidden doors
-            if (state == State.Closing)
+            if (closing)
             {
                 jailDoor1.transform.position = Vector3.MoveTowards(jailDoor1.transform.position, destination1.position, speed * Time.deltaTime);
                 jailDoor2.transform.position = Vector3.MoveTowards(jailDoor2.transform.position, destination2.position, speed * Time.deltaTime);
-
                 if (Vector3.Distance(jailDoor1.transform.position, destination1.position) < 0.1)
                 {
-                    state = State.Spawning;
+                    closing = false;
                 }
             }
-
-            if (state == State.Spawning)
-            { 
-                if (!spawning)
-                {
-                    StartSpawning();
-                    spawning = true;
-                }
-
-                //if (timer > 2)
-                //{
-                //    int deadEnemies = 0;
-                //    foreach (GameObject obj in enemies)
-                //    {
-                //        if (obj == null)
-                //            deadEnemies++;
-                //    }
-
-                //    if (deadEnemies >= enemiesToKill)
-                //    {
-                //        opening = true;
-                //        //    if (deadEnemies >= enemiesToKill)
-                //        //    {
-                //        //        opening = true;
-
-                //    }
-                //    timer = 0;
-                //}
-                //timer += Time.deltaTime;
-
-
-                //int deadEnemies = 0;
-                //for (int i = 0; i < spawnLocation.Length; i++)
-                //{
-                //    foreach (GameObject obj in enemySpawner.enemyArray)
-                //    {
-                //        Debug.Log("Enemies in array " + enemySpawner.enemyArray.Length);
-                //        if (obj == null)
-                //            deadEnemies++;
-                //    }
-                //}
-
-
-                //if (deadEnemies >= enemiesToKill)
-                //{
-                //    state = State.Opening;
-
-                //}
-                //timer = 0;
-
-
-                //timerTilDoorOpens -= Time.deltaTime;
-                //Debug.Log(timerTilDoorOpens);
-
-                //if (timerTilDoorOpens < 0)
-                //{
-                //    state = State.Opening;
-                //}
-
-                timer += Time.deltaTime;
-                Debug.Log(timer);
-
-                if (timer > 5)
-                {
-                    state = State.Opening;
-                }
-            }
-
-            //Opens the doors
-            if (state == State.Opening)
+            if (opening)
             {
+                
                 jailDoor1.transform.position = Vector3.MoveTowards(jailDoor1.transform.position, startPos1.position, speed * Time.deltaTime);
                 jailDoor2.transform.position = Vector3.MoveTowards(jailDoor2.transform.position, startPos2.position, speed * Time.deltaTime);
-
                 if (Vector3.Distance(jailDoor1.transform.position, startPos1.position) < 0.1)
                 {
-                    state = State.Completed;
+                    opening = false;
+                    complete = true;
                 }
             }
+            if (timer > 2)
+            {
+                int deadEnemies = 0;
+                foreach (GameObject obj in enemies)
+                {
+                    if (obj == null)
+                        deadEnemies++;
+                }
+                
+                if (deadEnemies >= enemiesToKill)
+                {
+                    opening = true;
+                    
+                }
+                timer = 0;
+            }
+            timer += Time.deltaTime;
         }
-    }
-
-    private void StartSpawning()
-    {
-        for (int i = 0; i < spawnLocation.Length; i++)
-        {
-            StartCoroutine(enemySpawner.SpawnWave(spawnLocation[i]));
-            //enemySpawner.SpawnEnemy();
-            //Debug.Log("Spawning");
-        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player") && state == State.Idle)
+        
+        if (other.gameObject.tag.Equals("Player"))
         {
-            state = State.Closing;
-            Debug.Log("Player has entered battle room");
+            
+            closing = true;
         }
     }
 }
